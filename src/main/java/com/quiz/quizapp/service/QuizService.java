a42fe53d02431db0a27936f29f0b7b28a3050a7a
Ahmad <ahmad477835@gmail.com>
2026-05-13 23:16:57 +0200

    implement getQuizById in the Service

--- src/main/java/com/quiz/quizapp/service/QuizService.java
+++ src/main/java/com/quiz/quizapp/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.quiz.quizapp.service;
 
 import com.quiz.quizapp.model.Question;
+import com.quiz.quizapp.model.QuestionWrapper;
 import com.quiz.quizapp.model.Quiz;
 import com.quiz.quizapp.repository.QuestionRepository;
 import com.quiz.quizapp.repository.QuizRepository;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,5 +32,19 @@ public class QuizService {
         quizRepository.save(quiz);
 
         return new ResponseEntity<>("created", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizById(Integer id) {
+        Quiz quiz = quizRepository.findById(id).orElseThrow();
+
+        List<Question> questionsFromDB = quiz.getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for (Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionDescription(), q.getOptionA(), q.getOptionB(), q.getOptionC(), q.getOptionD());
+
+            questionsForUser.add(qw);
+        }
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
