9e814f574c66440633a4df193c9ef2ca9a95fc43
Ahmad <ahmad477835@gmail.com>
2026-06-02 16:23:54 +0200

    add JavaDoc to QuizController.java

--- src/main/java/com/quiz/quizapp/controller/QuizController.java
+++ src/main/java/com/quiz/quizapp/controller/QuizController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller for managing quizzes
+ */
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
@@ -15,6 +18,13 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
+    /**
+     * Creates a new quiz based on the provided title, number of question and category.
+     * @param title The title of the new quiz.
+     * @param numQ The number of the questions the quiz should contain.
+     * @param category The category from witch the questions should be fetched.
+     * @return A ResponseEntity with success message and HTTP status.
+     */
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(
             @RequestParam String title,
@@ -24,6 +34,11 @@ public class QuizController {
         return quizService.createQuiz(title, numQ, category);
     }
 
+    /**
+     * Retrieves a specific quiz by its ID.
+     * @param id The identifier of the quiz to retrieve.
+     * @return A ResponseEntity containing a list of QuestionWrappers and HTTP status.
+     */
     @GetMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable Integer id){
         return quizService.getQuizById(id);
